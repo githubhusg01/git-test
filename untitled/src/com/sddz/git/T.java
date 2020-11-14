b868ff2dd3b2610a19052796b0a895e992ddbbf5
githubhusg01 <hushige@163.com>
2020-11-14 22:02:08 +0800

    clone commit push

--- untitled/src/com/sddz/git/T.java
+++ untitled/src/com/sddz/git/T.java
@@ -3,5 +3,12 @@ package com.sddz.git;
 public class T {
     public void aaa(){
         System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        //T
     }
 }
