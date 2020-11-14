9d732ec6b42f1a00e285beecb6088978e735ac40
githubhusg01 <hushige@163.com>
2020-11-14 22:05:33 +0800

    clone commit push222

--- untitled/src/com/sddz/git/Test.java
+++ untitled/src/com/sddz/git/Test.java
@@ -4,5 +4,11 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("xx");
+        System.out.println("TEST");
+
     }
 }
